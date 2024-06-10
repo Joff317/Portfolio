74a10c35a284291ea2651fb8ec50675596cd7061
Joff317 <j.freixanet@hotmail.com>
2024-06-10 15:04:47 +0200

    header init

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,5 +1,8 @@
+import { cn } from "@/lib/utils";
 import { PropsWithChildren } from "react";
 
-export const Section = ({ children }: { children: React.ReactNode }) => {
-  return <section className="max-w-3xl px-4 mx-auto">{children}</section>;
+export const Section = (props: PropsWithChildren<{ className?: string }>) => {
+  return (
+    <section className={cn("max-w-3xl px-4 mx-auto", props.className)}>{props.children}</section>
+  );
 };
