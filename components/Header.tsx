9f7e927c44c4896ec0a6baf46b4bac41ca89cc69
Joff317 <j.freixanet@hotmail.com>
2024-06-11 14:58:42 +0200

    responsive navbar v1

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import PageContainer from "./PageContainer";
 import HeaderNavigation from "./HeaderNavigation";
+import ResponsiveMenu from "./ResponsiveHeaderNavigation";
 
 export default function Header() {
   return (
     <header className="py-4">
       <PageContainer>
-        <div className="flex items-center justify-between w-full">
+        <div className="flex items-center justify-between w-full max-md:pl-2">
           <div className="flex items-center gap-2">
             {/* Responsive Menu */}
+            <ResponsiveMenu />
             <h1
               className="text-2xl font-bold"
               style={{
