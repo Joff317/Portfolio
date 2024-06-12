67d38fe91015078fcb9779de534ab728372cc1a4
Joff317 <j.freixanet@hotmail.com>
2024-06-12 16:08:06 +0200

    add front icon to design

--- components/SkillsSection.tsx
+++ components/SkillsSection.tsx
@@ -1,4 +1,9 @@
 import PageContainer from "./PageContainer";
+import { JavaScriptIcon } from "./icons/JavascriptIcon";
+import { NextIcon } from "./icons/NextIcon";
+import { ReactIcon } from "./icons/ReactIcon";
+import { TailwindIcon } from "./icons/TailwindIcon";
+import { TypeScriptIcon } from "./icons/TypeScriptIcon";
 
 export default function SkillsSection() {
   return (
@@ -9,11 +14,36 @@ export default function SkillsSection() {
           <h2 className="text-4xl px-4 mt-2">Front-end</h2>
           <div className="p-2">
             <ul className="px-4 text-3xl font-light flex flex-col gap-4">
-              <li>Javascript</li>
-              <li>React.js</li>
-              <li>Next.js</li>
-              <li>Typescript</li>
-              <li>Tailwind</li>
+              <li>
+                <div className="flex items-center gap-2">
+                  <JavaScriptIcon size={20} />
+                  <p>Javascript</p>
+                </div>{" "}
+              </li>
+              <li>
+                <div className="flex items-center gap-2">
+                  <ReactIcon size={20} />
+                  <p>React.js</p>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center gap-2">
+                  <NextIcon size={20} />
+                  <p>Next.js</p>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center gap-2">
+                  <TypeScriptIcon size={20} />
+                  <p>Typescript</p>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center gap-2">
+                  <TailwindIcon size={20} />
+                  <p>Tailwind</p>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
