3b27c50e25a4de6ee109d7bf187832d9097def18
Joff317 <j.freixanet@hotmail.com>
2024-06-13 15:40:33 +0200

    refacto project section for each card project

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,18 +1,13 @@
 import PageContainer from "./PageContainer";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+
 import { Button } from "./ui/button";
 import { JavaScriptIcon } from "./icons/JavascriptIcon";
 import { TailwindIcon } from "./icons/TailwindIcon";
 import { ReactIcon } from "./icons/ReactIcon";
 import { NodeIcon } from "./icons/NodeIcon";
 import SpacerCard from "./Card/SpacerCard";
+import PlantPediaCard from "./Card/PlantPediaCard";
+import SocialMooviesCard from "./Card/SocialMooviesCard";
 
 export default function Project() {
   return (
@@ -23,55 +18,10 @@ export default function Project() {
         <SpacerCard />
 
         {/* Deuxième carte */}
-        <div className="w-[350px] bg-bluecard border-slate-950 border-2 shadow-md rounded-xl h-full">
-          <CardHeader className="flex flex-row justify-between">
-            <CardTitle className="text-2xl text-white">🍀</CardTitle>
-            <CardDescription className="text-white">icon</CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-2">
-            <p className="text-2xl">Plantpedia</p>
-            <p>
-              A website to check properties of different plants around the world
-            </p>
-          </CardContent>
-          <CardFooter className="flex gap-2 text-sm flex-wrap">
-            <div className="flex items-center gap-2 border-none px-3 py-1 rounded-2xl bg-blue-500">
-              <ReactIcon size={14} /> React.js
-            </div>
-            <div className="flex items-center gap-2 border-none px-3 py-1 rounded-2xl bg-blue-500">
-              <NodeIcon size={14} /> Node.js
-            </div>
-            <div className="flex items-center gap-2 border-none px-3 py-1 rounded-2xl bg-blue-500">
-              <TailwindIcon size={14} /> Tailwind
-            </div>
-          </CardFooter>
-        </div>
+        <SocialMooviesCard />
 
         {/* Troisième carte */}
-        <div className="w-[350px] bg-bluecard border-slate-950 border-2 shadow-md rounded-xl h-auto">
-          <CardHeader className="flex flex-row justify-between">
-            <CardTitle className="text-2xl text-white">🎥</CardTitle>
-            <CardDescription className="text-white">icon</CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-2">
-            <p className="text-2xl">SocialMoovies</p>
-            <p>
-              A website where users can add audiovisuals, comment on them, add
-              them to favorites in order to get audiovisuals to watch
-            </p>
-          </CardContent>
-          <CardFooter className="flex gap-2 text-sm flex-wrap">
-            <div className="flex items-center gap-2 border-none px-3 py-1 rounded-2xl bg-blue-500">
-              <ReactIcon size={14} /> React.js
-            </div>
-            <div className="flex items-center gap-2 border-none px-3 py-1 rounded-2xl bg-blue-500">
-              <NodeIcon size={14} /> Node.js
-            </div>
-            <div className="flex items-center gap-2 border-none px-3 py-1 rounded-2xl bg-blue-500">
-              <TailwindIcon size={14} /> Tailwind
-            </div>
-          </CardFooter>
-        </div>
+        <PlantPediaCard />
       </div>
     </PageContainer>
   );
