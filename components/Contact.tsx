a95e5cff88fd3df530a4d06deef417e9aa26f75a
Joff317 <j.freixanet@hotmail.com>
2024-06-14 15:13:07 +0200

    waiting google for continue

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,11 +1,19 @@
+"use client";
 import PageContainer from "./PageContainer";
+import { Input } from "./ui/input";
 
 export default function Contact() {
   return (
     <PageContainer>
       <p>Contact</p>
 
-      
+      <form>
+        <Input type="email" placeholder="enter your mail" />
+
+        <Input type="text" placeholder="enter the subject of your mail" />
+
+        
+      </form>
     </PageContainer>
-  )
+  );
 }
---
+++ utils/sendEmail.tsx
@@ -0,0 +1,12 @@
+// import nodemailer from "nodemailer";
+
+// export interface EmailData {
+//   name: string;
+//   email: string;
+//   message: string;
+// }
+// export default function sendEmail({
+//   name,
+//   email,
+//   message,
+// }: EmailData): Promise<boolean> {}
