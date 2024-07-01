0814b4238fe797a63f2b5653e77b79902fca7082
Joff317 <j.freixanet@hotmail.com>
2024-07-01 12:30:19 +0200

    insert my picture

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,17 +7,18 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import businessman from "../public/businessman.png";
 import Link from "next/link";
 import Image from "next/image";
+import mypicture from "../public/IMG.png"
 
 export default function HeroSection() {
   return (
     <div className="flex flex-col gap-6 mx-auto px-2 py-4 max-sm:px-0">
       <div className="flex flex-col gap-2">
         <p className="text-lg text-text">Hello! I'm</p>
-        <div className="flex flex-row gap-2 my-2">
+        <div className="flex flex-row gap-2 my-2 items-center">
           <Image
-            src={businessman}
-            alt="Businessman"
-            className="w-[50px] h-[50px] max-md:w-[40px] max-md:h-[40px]"
+            src={mypicture}
+            alt="me"
+            className="w-[70px] h-[80px] max-md:w-[50px] max-md:h-[50px]"
           />
           <p className="text-5xl max-md:text-4xl text-text">
             Joffrey Freixanet
